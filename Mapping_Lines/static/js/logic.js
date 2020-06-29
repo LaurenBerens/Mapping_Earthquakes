c9e661440bacfdb231bbba341e0ad76e56516a4d
LaurenBerens <62680202+LaurenBerens@users.noreply.github.com>
2020-06-28 23:27:36 -0500

    final mapping lines

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,25 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Create tile layer for map background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/satellite-v9',
+    accessToken: API_KEY
+}).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);

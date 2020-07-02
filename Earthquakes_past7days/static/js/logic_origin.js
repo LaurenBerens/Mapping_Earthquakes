2507ad25eba62481cc01d080d9b8a587bddce45a
LaurenBerens <62680202+LaurenBerens@users.noreply.github.com>
2020-07-01 22:22:54 -0500

    earthquakes past 7 days

--- Earthquakes_past7days/static/js/logic_origin.js
+++ Earthquakes_past7days/static/js/logic_origin.js
@@ -17,15 +17,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  "Streets": streets,
-  "Satellite": satelliteStreets
-};
+    "Street": streets,
+    "Satellite Streets": satelliteStreets
+  };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [39.5, -98.5],
-	zoom: 3,
-	layers: [streets]
+	center: [43.7, -79.3],
+	zoom: 11,
+	layers: [satelliteStreets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -40,8 +40,9 @@ let torontoData = "https://raw.githubusercontent.com/LaurenBerens/Mapping_Earthq
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/LaurenBerens/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
-// Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+
+L.geoJSON(data).addTo(map);
 });
